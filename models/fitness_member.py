69e88d36f9f6a11d900e533865eb627cb0622582
JaySkyscend <jaypatel.skyscendbs@gmail.com>
2025-03-25 11:46:48 +0530

    Added Header

--- models/fitness_member.py
+++ models/fitness_member.py
@@ -1,4 +1,5 @@
 
+
 from odoo import models, fields
 
 class FitnessMember(models.Model):
@@ -8,12 +9,12 @@ class FitnessMember(models.Model):
 
 
     name = fields.Char(string="Member Name",required=True,placeholder="Enter Full Name")
-    age = fields.Integer(string="Age")
+    age = fields.Integer(string="Age", required=True)
     weight = fields.Float(string="Weight (kg)",digits=(6,3))
     is_active = fields.Boolean(string="Active",default=True)
     address = fields.Text(string="Address")
     health_details = fields.Html(string="Health Details",)
-    join_date = fields.Date(string='Join Date')
+    join_date = fields.Date(string='Join Date',default=fields.Date.context_today,index=True)
     last_visit = fields.Datetime(string="Last Visit")
     membership_type = fields.Selection([
         ('bronze','Bronze'),
@@ -28,5 +29,6 @@ class FitnessMember(models.Model):
     priority = fields.Selection([(str(num),str(num)) for num in range(0,5)], 'Priority')
     workout_time = fields.Float(string="Workout Time (HH:MM)",help="Example: 10:30, 15:45, 9:00")
 
+    membership_code = fields.Char(string="Membership Code",size=4)
     password = fields.Char(string="Password",password=True)
 
