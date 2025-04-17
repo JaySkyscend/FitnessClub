ab80183cbdb2f05086634157484f2b63f9805743
JaySkyscend <jaypatel.skyscendbs@gmail.com>
2025-04-17 17:06:41 +0530

    [FIX] Fix sequence in __manifest__.py and __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from . import fitness_member
-from . import fitness_trainer
 from . import fitnesss_certification
+from . import fitness_trainer
 from . import fitness_session
 from . import fitness_equipment
 
